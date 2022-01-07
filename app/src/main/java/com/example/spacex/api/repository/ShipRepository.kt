ab05069841de676d42fcd5e6cdd35ec056f20082
mariamikv <mariamikv71@gmail.com>
2022-01-07 22:56:42 +0400

    update  gradle

--- app/src/main/java/com/example/spacex/api/repository/ShipRepository.kt
+++ app/src/main/java/com/example/spacex/api/repository/ShipRepository.kt
@@ -1,34 +1,9 @@
 package com.example.spacex.api.repository
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.example.spacex.api.request.ShipService
-import com.example.spacex.models.Ship
-import com.example.spacex.utils.Result
 
 class ShipRepository(private val shipService: ShipService) {
     suspend fun getShipList() = shipService.shipList()
-
-//    suspend fun getShipList(): MutableLiveData<List<Ship>> {
-//        val shipLiveData = MutableLiveData<List<Ship>>().apply {
-//            mutableListOf<Ship>()
-//        }
-//        val _shipLiveData: LiveData<List<Ship>> = shipLiveData
-//
-//        val response = shipService.shipList()
-//
-//        if(response.isSuccessful){
-//            Result.Success(response.body())
-//        }else{
-//            val errorMsg = response.errorBody()?.toString()
-//            response.errorBody()?.close()
-//            if (errorMsg != null) {
-//                Result.Error(errorMsg)
-//            }
-//        }
-//
-//        return shipLiveData
-//    }
 
     suspend fun getMissionList() = shipService.missionList()
 }
